8f56192b52abca56b55c0cf6325df0f28f5a7396
SilasAlvesTi <silas.ifce.ti@gmail.com>
2024-05-28 17:52:51 -0300

    Initial commit

---
+++ Fina.Core/Requests/Transactions/GetTransactionsByPeriod.cs
@@ -0,0 +1,8 @@
+namespace Fina.Core.Requests.Transactions
+{
+    public class GetTransactionsByPeriod : PagedRequest
+    {
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set;}
+    }
+}

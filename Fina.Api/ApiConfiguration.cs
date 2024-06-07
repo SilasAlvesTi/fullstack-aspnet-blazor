791f8883557633cf5311586aaa8f916fc5e0013e
SilasAlvesTi <silas.ifce.ti@gmail.com>
2024-06-07 14:39:08 -0300

    API finalizada

---
+++ Fina.Api/ApiConfiguration.cs
@@ -0,0 +1,9 @@
+namespace Fina.Api
+{
+    public static class ApiConfiguration
+    {
+        public const string UserId = "silas@gmail.com";
+        public static string ConnectionString { get; set; } = string.Empty;
+        public static string CorsPolicyName = "wasm";
+    }
+}
